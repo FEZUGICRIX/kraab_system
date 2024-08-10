5de0e2bd4216054c53de0a9125df22ebad900fbe
unknown <redanom@mail.ru>
2024-08-10 19:06:18 +0500

    feat: add functions fetch for each payment method

---
+++ src/utils/checkPaymentStatus/fetchRevolutData.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import { sendEmail } from '../../api/sendEmail';
+
+export const fetchRevolutData = async ({
+  orderData,
+  paymentId,
+  emailSent,
+  setEmailSent,
+  setBasketItems,
+  basketProducts,
+  basketItemsMap,
+  totalPrice,
+  shippingValue
+}) => {
+  try {
+    setBasketItems([]);
+    const response = await axios.post(
+      'https://kraabmod.fi/api/revolut_confirmation.php',
+      { order_id: paymentId }
+    );
+
+    if (
+      response.data &&
+      response.data.state === 'completed' &&
+      !emailSent
+    ) {
+      if (!orderData) {
+        console.error('Нет данных заказа в localStorage');
+        return;
+      }
+
+      const { fullName, email, phone, address, state, city, zip } = orderData;
+
+      const orderDataToSend = {
+        fullName,
+        email,
+        phone,
+        address,
+        state,
+        city,
+        zip,
+        time: response.data.updated_at,
+        shippingValue,
+        totalPrice: (totalPrice + shippingValue).toFixed(2),
+        items: basketProducts.map((item) => {
+          const basketItem = basketItemsMap[item.id];
+          return {
+            ...item,
+            packages: basketItem?.packages || null,
+          };
+        }),
+      };
+
+      if (!emailSent) {
+        await sendEmail({ orderData: orderDataToSend });
+        setEmailSent(true);
+      }
+    }
+  } catch (error) {
+    console.error('Ошибка при получении данных заказа:', error);
+  }
+};
